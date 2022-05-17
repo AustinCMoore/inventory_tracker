d7efc6bd49684e2b73dc52d9429eaa1c7a961803
Austin Moore <austinchristianmoore@gmail.com>
2022-05-17 17:48:44 -0400

    Feature: Create happy path item creation

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,12 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def create
+    Item.create!(name: params[:item_name])
+    redirect_to "/items"
+  end
+
+  def new
+  end
 end
